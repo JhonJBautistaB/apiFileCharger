6a789a6e77e1ff25638b64759e864cdc52b8fe19
JhonJBautistaB <jhon.bautista@ingenieros.com>
2021-01-25 18:21:01 -0500

    modificaciones en docker compose

--- fileCharger/settings.py
+++ fileCharger/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
         'NAME': 'chargerFile',
         'USER': 'postgres',
         'PASSWORD': 'postgres',
-        'HOST': 'localhost',
+        'HOST': 'db',
         'PORT': 5432,
     }
 }
